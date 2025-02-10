83716c544da7d0c92fe820893d85deb259ad1ed9
az <az.co.jp@icloud.com>
2025-02-11 06:31:09 +0900

    フィードバック画面の文字サイズ調整

--- src/App.tsx
+++ src/App.tsx
@@ -187,9 +187,9 @@ function App() {
             </div>
 
             {next ? (
-              <div className="feedback-sec flex-2 my-5 text-4xl font-semibold">
+              <div className="feedback-sec flex-2 my-5 text-3xl font-semibold">
                 <h2
-                  className={`text-5xl py-5 ${
+                  className={`text-3xl py-5 ${
                     feedback == "正解!!" ? "text-[#ff5432]" : "text-[#5888ff]"
                   }`}
                 >
